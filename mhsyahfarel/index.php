f84112a62cffbafab1da10450c0ad63dc10bbaff
MHSyahfarel <muhammadsyahfarel@gmail.com>
2022-07-22 23:09:57 +0700

    first commit

---
+++ mhsyahfarel/index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <!-- My CSS -->
+ <link rel="stylesheet" href = "style.css"/ >
+
+    <title>Biography | Stephen Curry</title>
+    
+    
+  </head>
+  <body id="beranda">
+   <!-- Navbar -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow-sm">
+  <div class="container">
+    <a class="navbar-brand" href="#">mhsyahfarel</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#beranda">Beranda</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="#biografi">Biografi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#kontak">Kontak</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#tentang saya">Tentang Saya</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+   <!-- Akhir Navbar -->
+
+
+<!-- Projects -->
+<section id="biografi">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col bg-success text-white">   
+      <h2>Raja Tiga Poin di NBA</h2>     
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4 mb-3">
+      <div class="card bg-success">
+  <img src="img/Stephen-Curry.jpg" class="card-img-top" alt="Stephen Curry">
+  <div class="card-body">
+    <p class="card-text-center fw-bold text-white">Stephen Curry</p>
+  </div>
+</div>
+</div>
+<div class="col-text-align:justify">Wardell Stephen Curry (lahir 14 Maret 1988) adalah Pemain Bola Basket Profesional Amerika dari Akron, Ohio. Dia saat ini mewakili Golden State Warriors di NBA. Selanjutnya, pemain berusia 30 tahun ini dipilih oleh Golden State Warriors pada 2009.
+
+Ia memilih sebagai keseluruhan ketujuh dalam draft NBA 2009. Selain itu, ia menunjukkan keterampilan menembaknya sejak awal. Curry juga mencetak rekor 166 tiga poin sebagai rookie di liga. Bahkan, pemain itu memenangkan " NBA Most Valuable Player Award " dua kali berturut-turut. 
+Pada bulan Februari 2014, Steph membuat penampilan All-Star pertamanya untuk Barat. Stephen kemudian dipilih untuk Tim All-NBA pertamanya. Pemain ini juga menjadi pencetak angka tiga angka terkemuka untuk Warriors. Dia memecahkan rekor liga sendiri dengan meraih 286 tembakan tiga angka.
+
+Baru-baru ini, Curry mencetak 51 poin dan berada di peringkat kelima dalam daftar 3-poin NBA sepanjang masa. Selain itu, ia merobohkan 11 treys dalam kemenangan 144-122 atas Washington Wizards pada 24 Oktober 2018.
+Pemain NBA Bintang ini lahir pada 14 Maret 1988 di Akron, Ohio, Amerika Serikat. Oleh karena itu, usia Stephen Curry adalah Tiga Puluh (30) tahun, sejak 2018. Ayahnya "Dell Curry" adalah mantan pemain NBA dan ibunya "Sonya" bermain bola voli di Virginia Tech.
+
+Selanjutnya, saudaranya yang bernama Seth juga merupakan pemain bola basket profesional yang mewakili Dallas Mavericks. Adik Stephen bernama " Sydel Curry " bermain bola voli di Elon University. Pemain menghadiri Charlotte Christian High School untuk pendidikannya.
+
+Selain itu, Curry kemudian mendaftar di Davidson College pada November 2005. Namun, ia meninggalkan College tanpa menyelesaikan tahun seniornya untuk bermain NBA.
+
+Pada 30 Juli 2011, Steph Curry menikahi kekasihnya di perguruan tinggi "Ayesha Alexander" di Charlotte. Pasangan itu pertama kali bertemu dalam kelompok pemuda gereja ketika mereka masih remaja. Pasangan ini juga berbagi dua putri, Ryan & Riley dan seorang putra Canon.</div>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2E8B57" fill-opacity="1" d="M0,128L40,144C80,160,160,192,240,197.3C320,203,400,181,480,149.3C560,117,640,75,720,53.3C800,32,880,32,960,69.3C1040,107,1120,181,1200,197.3C1280,213,1360,171,1400,149.3L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+</section>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2E8B57" fill-opacity="1" d="M0,128L40,144C80,160,160,192,240,224C320,256,400,288,480,277.3C560,267,640,213,720,160C800,107,880,53,960,64C1040,75,1120,149,1200,192C1280,235,1360,245,1400,250.7L1440,256L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
+
+<!-- Akhir Projects -->
+
+<!-- Contact -->
+<section id="kontak">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>Kontak Saya</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+      <form>
+  <div class="mb-3">
+    <label for="nama" class="form-label">Nama Lengkap</label>
+    <input type="text" class="form-control" id="nama" aria-describedby="nama">
+  </div>
+  <div class="mb-3">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" aria-describedby="email">
+  </div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
+  <textarea class="form-control" id="pesan" rows="3"></textarea>
+</div>
+  <button type="submit" class="btn btn-primary">Kirim</button>
+</form>
+      </div>
+    </div>
+  </div>
+ 
+</section>
+<!-- Akhir Contact -->
+
+<!-- About -->
+<!-- Jumbotron -->
+<section id="tentang saya">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bcf160" fill-opacity="1" d="M0,160L40,170.7C80,181,160,203,240,186.7C320,171,400,117,480,101.3C560,85,640,107,720,101.3C800,96,880,64,960,90.7C1040,117,1120,203,1200,202.7C1280,203,1360,117,1400,74.7L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+<section class="jumbotron text-center">
+<div class="col">
+<h2>Tentang Saya</h2>
+      </div>
+    <img src = "img/mhsyahfarel.jpg" alt = "Syrelz" width = "200"
+    class="rounded-circle img-thumbnail"/>
+  <h1 class="display-4">Muhammad Haikal Syahfarel</h1>
+  <p class="lead"> Mahasiswa UMRI | Sistem Informasi</p>
+
+  <div class="container">
+    <div class="row text-center mb-3">
+    </div>
+    <div class="row justify-content-center fs-6 text-center pb-90">
+      <div class="col-md-4">
+      <p>Saya merupakan mahasiswa UMRI (Universitas Muhammadiyah Riau). Fakultas yang saya pilih adalah Ilmu Komputer, sedangkan Program Studi yang saya pilih adalah Sistem Informasi. Jika ingin mengetahui tentang saya silahkan hubungi/chat saya. Terima Kasih.</p>
+      </div>
+    </div>
+  </div>
+ </section>
+<!-- Akhir About -->
+</section>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bcf160" fill-opacity="1" d="M0,160L40,170.7C80,181,160,203,240,186.7C320,171,400,117,480,101.3C560,85,640,107,720,101.3C800,96,880,64,960,90.7C1040,117,1120,203,1200,202.7C1280,203,1360,117,1400,74.7L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
+  <!-- Akhir Jumbotron -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
+
+<!-- Akhir Jumbotron -->
+
+
+<!-- Footer -->
+
+<footer class="bg-success text-white text-center pb-1">
+
+  <p>Created by <a href="https://www.instagram.com/haykalsyh" class="text-white fw-bold">Muhammad Haikal Syahfarel</a></p>
+</footer>
+<!-- Akhir Footer -->
